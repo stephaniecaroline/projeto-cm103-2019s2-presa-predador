635521d5bf456b1b52305a1035f7c4ebf2edd69a
stephaniecaroline <54449173+stephaniecaroline@users.noreply.github.com>
2019-11-22 14:05:22 -0300

    Update presa-predador.jl

--- presa-predador.jl
+++ presa-predador.jl
@@ -31,7 +31,7 @@ plot(U1[1,:],U1[2,:],label="Condição 1",xlabel="Presas",ylabel="Predadores")
 plot!(U2[1,:],U2[2,:],label="Condição 2")
 plot!(U3[1,:],U3[2,:],label="Condição 3") 
 
-##vetores os os dados observados na tabela
+##vetores os dos dados observados na tabela
 c = [30 ; 47.2 ; 70.2 ; 77.4 ; 36.3 ; 20.6 ; 18.1; 21.4; 22; 25.4; 27.1; 40.3; 57; 76.6; 52.3; 19.5; 11.2; 7.6; 14.6; 16.2; 24.7]
 l = [4; 6.1; 9.8; 35.2; 59.4; 41.7; 19; 13; 8.3; 9.1; 7.4; 8; 12.3; 19.5; 45.7; 51.1; 29.7; 15.8; 9.7; 10.1; 8.6]
 
